b2ac4022b9c896c9d84f40282e702507c8eecabf
Aaron ke <b.aaron.ke@gmail.com>
2021-05-25 02:12:39 +0800

    Guess number game final version

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,14 @@
 import random
+start = input('請決定隨機整數範圍開始值:')
+end = input('請決定隨機整數範圍結束值:')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0 
 while True:
 	count += 1 # count += 1 = (count = count +1)
-	answer = input('1到100猜一個數字:')
+	answer = input('猜範圍中的一個數字:')
 	answer = int(answer)
 	if answer == r:
 		print('恭喜你猜對了')
